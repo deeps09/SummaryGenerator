05fdf78948db60aa9546d84b6f1f49c70312f00f
Gupta1 <128953@NTTDATA.COM>
2018-03-01 17:02:20 +0530

    Final version of excel utility

---
+++ SummaryGenerator/Form1.Designer.cs
@@ -0,0 +1,190 @@
+﻿namespace SummaryGenerator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnAdd_Incidents = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.rbMs4 = new System.Windows.Forms.RadioButton();
+            this.rbInitiate = new System.Windows.Forms.RadioButton();
+            this.txtSrcFilePath = new System.Windows.Forms.TextBox();
+            this.txtDestFilePath = new System.Windows.Forms.TextBox();
+            this.btnSrcFileDialog = new System.Windows.Forms.Button();
+            this.btnDestFileDialog = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnAdd_Incidents
+            // 
+            this.btnAdd_Incidents.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAdd_Incidents.Location = new System.Drawing.Point(129, 341);
+            this.btnAdd_Incidents.Name = "btnAdd_Incidents";
+            this.btnAdd_Incidents.Size = new System.Drawing.Size(276, 57);
+            this.btnAdd_Incidents.TabIndex = 0;
+            this.btnAdd_Incidents.Text = "Update Incident Tracker";
+            this.btnAdd_Incidents.UseVisualStyleBackColor = true;
+            this.btnAdd_Incidents.Click += new System.EventHandler(this.btnAdd_Incidents_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(157, 63);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(216, 24);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "  Updating Incidents for:  ";
+            // 
+            // rbMs4
+            // 
+            this.rbMs4.AutoSize = true;
+            this.rbMs4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rbMs4.Location = new System.Drawing.Point(184, 95);
+            this.rbMs4.Name = "rbMs4";
+            this.rbMs4.Size = new System.Drawing.Size(64, 24);
+            this.rbMs4.TabIndex = 2;
+            this.rbMs4.TabStop = true;
+            this.rbMs4.Text = "MS4";
+            this.rbMs4.UseVisualStyleBackColor = true;
+            this.rbMs4.CheckedChanged += new System.EventHandler(this.rbMs4_CheckedChanged);
+            // 
+            // rbInitiate
+            // 
+            this.rbInitiate.AutoSize = true;
+            this.rbInitiate.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rbInitiate.Location = new System.Drawing.Point(263, 95);
+            this.rbInitiate.Name = "rbInitiate";
+            this.rbInitiate.Size = new System.Drawing.Size(79, 24);
+            this.rbInitiate.TabIndex = 3;
+            this.rbInitiate.TabStop = true;
+            this.rbInitiate.Text = "Initiate";
+            this.rbInitiate.UseVisualStyleBackColor = true;
+            this.rbInitiate.CheckedChanged += new System.EventHandler(this.rbInitiate_CheckedChanged);
+            // 
+            // txtSrcFilePath
+            // 
+            this.txtSrcFilePath.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtSrcFilePath.Location = new System.Drawing.Point(59, 186);
+            this.txtSrcFilePath.Name = "txtSrcFilePath";
+            this.txtSrcFilePath.Size = new System.Drawing.Size(349, 24);
+            this.txtSrcFilePath.TabIndex = 4;
+            // 
+            // txtDestFilePath
+            // 
+            this.txtDestFilePath.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtDestFilePath.Location = new System.Drawing.Point(59, 259);
+            this.txtDestFilePath.Name = "txtDestFilePath";
+            this.txtDestFilePath.Size = new System.Drawing.Size(349, 24);
+            this.txtDestFilePath.TabIndex = 5;
+            // 
+            // btnSrcFileDialog
+            // 
+            this.btnSrcFileDialog.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSrcFileDialog.Location = new System.Drawing.Point(415, 181);
+            this.btnSrcFileDialog.Name = "btnSrcFileDialog";
+            this.btnSrcFileDialog.Size = new System.Drawing.Size(75, 34);
+            this.btnSrcFileDialog.TabIndex = 6;
+            this.btnSrcFileDialog.Text = "Browse";
+            this.btnSrcFileDialog.UseVisualStyleBackColor = true;
+            this.btnSrcFileDialog.Click += new System.EventHandler(this.btnSrcFileDialog_Click);
+            // 
+            // btnDestFileDialog
+            // 
+            this.btnDestFileDialog.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDestFileDialog.Location = new System.Drawing.Point(414, 255);
+            this.btnDestFileDialog.Name = "btnDestFileDialog";
+            this.btnDestFileDialog.Size = new System.Drawing.Size(75, 32);
+            this.btnDestFileDialog.TabIndex = 7;
+            this.btnDestFileDialog.Text = "Browse";
+            this.btnDestFileDialog.UseVisualStyleBackColor = true;
+            this.btnDestFileDialog.Click += new System.EventHandler(this.btnDestFileDialog_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(55, 165);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(275, 18);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Source file path (From Service Matters) :";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(56, 236);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(263, 18);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Destination file path (Incident Tracker) :";
+            // 
+            // Form1
+            // 
+            this.AcceptButton = this.btnAdd_Incidents;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(556, 463);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btnDestFileDialog);
+            this.Controls.Add(this.btnSrcFileDialog);
+            this.Controls.Add(this.txtDestFilePath);
+            this.Controls.Add(this.txtSrcFilePath);
+            this.Controls.Add(this.rbInitiate);
+            this.Controls.Add(this.rbMs4);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnAdd_Incidents);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Incident Tracker";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnAdd_Incidents;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.RadioButton rbMs4;
+        private System.Windows.Forms.RadioButton rbInitiate;
+        private System.Windows.Forms.TextBox txtSrcFilePath;
+        private System.Windows.Forms.TextBox txtDestFilePath;
+        private System.Windows.Forms.Button btnSrcFileDialog;
+        private System.Windows.Forms.Button btnDestFileDialog;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ SummaryGenerator/Form1.cs
@@ -0,0 +1,312 @@
+﻿using System.Configuration;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.OleDb;
+using Microsoft.CSharp;
+using Excel = Microsoft.Office.Interop.Excel;
+using MyMarshal = System.Runtime.InteropServices.Marshal;
+
+
+namespace SummaryGenerator
+{
+    public partial class Form1 : Form
+    {
+
+        // keys stored in app.config file 
+        const String MS4_SRC_FILE_KEY = "ms4SrcFile", MS4_DEST_FILE_KEY = "ms4DestFile", INI_SRC_FILE_KEY = "initiateSrcFile", INI_DEST_FILE_KEY = "initiateDestFile";
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnAdd_Incidents_Click(object sender, EventArgs e)
+        {
+            DataTable sourceDataTable = loadDataFromSourceExcel();
+
+            if (sourceDataTable.Rows.Count <= 0) {
+                MessageBox.Show("No records found.. Aborting..","Something went wrong", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            Configuration config = ConfigurationManager.OpenExeConfiguration(Application.ExecutablePath);
+
+            if (rbMs4.Checked)
+            {   
+                config.AppSettings.Settings[MS4_SRC_FILE_KEY].Value = txtSrcFilePath.Text;
+                config.AppSettings.Settings[MS4_DEST_FILE_KEY].Value = txtDestFilePath.Text;
+            }
+
+            if (rbInitiate.Checked)
+            {
+                config.AppSettings.Settings[INI_SRC_FILE_KEY].Value = txtSrcFilePath.Text;
+                config.AppSettings.Settings[INI_DEST_FILE_KEY].Value = txtDestFilePath.Text;
+            }
+            config.Save(ConfigurationSaveMode.Modified);
+
+            updateIncidentTracker(sourceDataTable, txtDestFilePath.Text); 
+        }
+
+        private DataTable loadDataFromSourceExcel() {
+
+            String _null = "";
+            String srcSheetName = "Page 1$";
+            //String srcConnStr = @"Provider=Microsoft.ACE.OLEDB.12.0;Data Source=C:\Users\..\Desktop\ServiceMattersIncidentTool\incident.xlsx;Extended Properties=""Excel 12.0 Xml;HDR=Yes;""";
+            String srcConnStr = @"Provider=Microsoft.ACE.OLEDB.12.0;Data Source=" + txtSrcFilePath.Text + @";Extended Properties=""Excel 12.0 Xml;HDR=Yes;""";
+            String selCommandText = "SELECT * FROM ['" + srcSheetName + "'] WHERE [Number] <> '" + _null + "' ORDER BY [Number]";
+
+            Cursor.Current = Cursors.WaitCursor;
+            DataTable dataTable = new DataTable();
+            OleDbConnection srcFileConnection = new OleDbConnection(srcConnStr);
+            OleDbCommand oleDbCmnd = new OleDbCommand();
+            OleDbDataAdapter oleDataAdapter = new OleDbDataAdapter();
+
+            try
+            {
+                srcFileConnection.Open();
+                oleDbCmnd.Connection = srcFileConnection;
+                oleDbCmnd.CommandText = selCommandText;
+
+                oleDataAdapter.SelectCommand = oleDbCmnd;
+                oleDataAdapter.Fill(dataTable);
+                //dataSet.Tables.Add(dataTable);
+                //dataTable.Columns["Close Notes"].ColumnName = "CloseNotes";
+                //dataTable.Columns.Add("NoteLength", typeof(int), "len(CloseNotes)");
+                //dataTable.DefaultView.Sort = "NoteLength desc";
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.Message);
+                Cursor.Current = Cursors.Default;
+            }
+            finally {
+                oleDbCmnd = null;
+                srcFileConnection.Close();
+            }
+            return dataTable;
+        }
+
+        private void updateIncidentTracker(DataTable srcDataset, String destFilePath) {
+
+            String destSheetName = "Incident Tracker$"; // Destination worksheet name in excel
+            DataTable destDataSet =  getAllIncidentsFromDestFile(destSheetName, destFilePath);
+
+            Excel.Workbook MyBook = null;
+            Excel.Workbooks xlWorkBooks = null;
+            Excel.Application MyApp = null;
+            Excel.Worksheet MySheet = null;
+
+            MyApp = new Excel.Application();
+            MyApp.Visible = false;
+            xlWorkBooks = MyApp.Workbooks;
+            MyBook = xlWorkBooks.Open(destFilePath);
+            MySheet = MyBook.Sheets[7];
+
+            // variable declaration
+            String incidentNumber, incidentDesc, severity, status, resolutionSummary, assigneeIndividual, 
+                customerName, incidentOpenDt, incidentActualStartDt, incidentResolvedDt, assigneeGroup;
+
+            String existingState;
+
+            int noOfIncidentsUpdated = 0, noOfIncidentsAdded = 0, noOfIncidentsSkipped = 0;
+            int countOfIncidentFound;
+
+            DataRow[] foundRows;
+            int totalCount = destDataSet.Rows.Count + 2; // one for blank row and one for getting new row for intertion
+
+            try
+            {
+                foreach (DataRow dataRow in srcDataset.Rows)
+                {
+                    countOfIncidentFound = -1;
+
+                    incidentNumber = dataRow["Number"].ToString();
+                    resolutionSummary = dataRow["Close notes"].ToString().Replace("'", "''");
+                    customerName = dataRow["Customer"].ToString();
+                    incidentDesc = dataRow["Short description"].ToString().Replace("'", "''");
+                    severity = dataRow["Priority"].ToString();
+                    status = dataRow["State"].ToString();
+                    assigneeGroup = dataRow["Assignment group"].ToString();
+                    assigneeIndividual = dataRow["Assigned to"].ToString();
+                    incidentOpenDt = dataRow["Created"].ToString();
+                    incidentActualStartDt = dataRow["Actual start"].ToString();
+                    incidentResolvedDt = dataRow["Resolved"].ToString();
+
+                    if (incidentResolvedDt == String.Empty)
+                        incidentResolvedDt = DateTime.MinValue.ToString();
+
+                    foundRows = destDataSet.Select("F1 = '" + incidentNumber + "'");
+
+
+                    if (foundRows.Count() > 0)
+                        countOfIncidentFound = destDataSet.Rows.IndexOf(foundRows[0]);
+
+                    if (countOfIncidentFound <= 0)//countOfIncidentFound < 1) // No match found hence insertion will be performed
+                    {
+
+                        MySheet.Cells[totalCount, 1] = incidentNumber;
+                        MySheet.Cells[totalCount, 3] = incidentDesc;
+                        MySheet.Cells[totalCount, 4] = severity;
+                        MySheet.Cells[totalCount, 8] = status;
+                        MySheet.Cells[totalCount, 9] = resolutionSummary;
+                        MySheet.Cells[totalCount, 10] = assigneeIndividual;
+                        MySheet.Cells[totalCount, 11] = assigneeIndividual;
+                        MySheet.Cells[totalCount, 12] = assigneeIndividual;
+                        MySheet.Cells[totalCount, 13] = customerName;
+                        MySheet.Cells[totalCount, 14] = incidentOpenDt;
+                        MySheet.Cells[totalCount, 16] = incidentResolvedDt;
+
+                        totalCount++;
+                        noOfIncidentsAdded++;
+                    }
+                    else
+                    { // match found hence update will be performed
+
+                        //MySheet.Cells[countOfIncidentFound + 2, 1] = incidentNumber;
+                        //MySheet.Cells[countOfIncidentFound + 2, 9] = resolutionSummary;
+
+                        countOfIncidentFound += 2;
+                        existingState = (MySheet.Cells[countOfIncidentFound, 8]).Value.ToString();
+
+                        if (existingState != "Closed")
+                        {
+                            MySheet.Cells[countOfIncidentFound, 1] = incidentNumber;
+                            MySheet.Cells[countOfIncidentFound, 3] = incidentDesc;
+                            MySheet.Cells[countOfIncidentFound, 4] = severity;
+                            MySheet.Cells[countOfIncidentFound, 8] = status;
+                            MySheet.Cells[countOfIncidentFound, 9] = resolutionSummary;
+                            MySheet.Cells[countOfIncidentFound, 10] = assigneeIndividual;
+                            MySheet.Cells[countOfIncidentFound, 11] = assigneeIndividual;
+                            MySheet.Cells[countOfIncidentFound, 12] = assigneeIndividual;
+                            MySheet.Cells[countOfIncidentFound, 13] = customerName;
+                            MySheet.Cells[countOfIncidentFound, 14] = incidentOpenDt;
+                            MySheet.Cells[countOfIncidentFound, 16] = incidentResolvedDt;
+
+                            noOfIncidentsUpdated++;
+                        }
+                        else {
+                            noOfIncidentsSkipped++;
+                        }
+                        
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.Message , "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            finally {
+                MyBook.Save();
+
+                GC.Collect();
+                GC.WaitForPendingFinalizers();
+
+                MyBook.Close(false, destFilePath, null);
+                MyApp.Quit();
+
+                MyMarshal.ReleaseComObject(MySheet);
+                MyMarshal.ReleaseComObject(xlWorkBooks);
+                MyMarshal.ReleaseComObject(MyBook);
+                MyMarshal.ReleaseComObject(MyApp);
+            }
+
+            int totalTkts = noOfIncidentsUpdated + noOfIncidentsAdded + noOfIncidentsSkipped;
+            Cursor.Current = Cursors.Default;
+            MessageBox.Show("Tracker has been updated successfully with: " +
+                "\n Rows Added: "+ noOfIncidentsAdded + " " +
+                "\n Rows Updated: "+ noOfIncidentsUpdated +
+                "\n Rows Skipped: "+ noOfIncidentsSkipped +"" +
+                "\n Total Rows: "+ totalTkts + "","Status", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+        }
+
+        private DataTable getAllIncidentsFromDestFile(String destSheetName, String destFilePath)
+        {
+            String _null = "";
+            String selectQuery = "SELECT F1 FROM ['" + destSheetName + "'] WHERE [F1] <> '" + _null + "'";
+            //String destConnStr = @"Provider=Microsoft.ACE.OLEDB.12.0;
+            //                      Data Source= C:\Users\128953\Desktop\ServiceMattersIncidentTool\MS4_IncidentTracker_QR202_2018.xlsm;
+            //                      Extended Properties='Excel 12.0 XML;HDR=NO'";
+            String destConnStr = @"Provider=Microsoft.ACE.OLEDB.12.0;
+                                  Data Source= '" + destFilePath + "';" +
+                                  @"Extended Properties='Excel 12.0 XML;HDR=NO'";
+
+            OleDbConnection con = new OleDbConnection(destConnStr);
+            OleDbCommand cmd = new OleDbCommand();
+            OleDbDataAdapter sda = new OleDbDataAdapter();
+            DataTable dt = new DataTable();
+
+            try
+            {
+                con.Open();
+                cmd.CommandText = selectQuery;
+                cmd.Connection = con;
+                
+                sda.SelectCommand = cmd;
+                sda.Fill(dt);
+            }
+            catch (Exception e) {
+            }
+            finally {
+                cmd = null;
+                con.Close();
+            }
+
+            return dt;
+        }
+
+        private void btnSrcFileDialog_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "Excel Files|*.xls;*.xlsx;*.xlsm;";
+            openFileDialog.InitialDirectory = txtSrcFilePath.Text;
+
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                txtSrcFilePath.Text = openFileDialog.FileName;
+            }
+        }
+
+        private void btnDestFileDialog_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "Excel Files|*.xls;*.xlsx;*.xlsm;";
+            openFileDialog.InitialDirectory = txtDestFilePath.Text;
+
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                txtDestFilePath.Text = openFileDialog.FileName;
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            rbMs4.Checked = true;
+        }
+
+        private void rbMs4_CheckedChanged(object sender, EventArgs e)
+        {
+            if (rbMs4.Checked)
+            {
+                txtSrcFilePath.Text = ConfigurationManager.AppSettings.Get(MS4_SRC_FILE_KEY);
+                txtDestFilePath.Text = ConfigurationManager.AppSettings.Get(MS4_DEST_FILE_KEY);
+            }
+        }
+
+        private void rbInitiate_CheckedChanged(object sender, EventArgs e)
+        {
+            if (rbInitiate.Checked)
+            {
+                txtSrcFilePath.Text = ConfigurationManager.AppSettings.Get(INI_SRC_FILE_KEY);
+                txtDestFilePath.Text = ConfigurationManager.AppSettings.Get(INI_DEST_FILE_KEY);
+            }
+        }
+    }
+}
